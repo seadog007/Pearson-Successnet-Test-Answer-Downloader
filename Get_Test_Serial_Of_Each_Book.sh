73d4596f06de3636c72d00fb15424f8febf1a22f
seadog007 <007seadog@gmail.com>
2015-02-14 01:45:26 +0800

    Add Get Test Serial Of Each Book

---
+++ Get_Test_Serial_Of_Each_Book.sh
@@ -0,0 +1,22 @@
+ISBN=$1
+SID=$2
+while read line
+do
+
+	[ "$line" == "MT" ] && continue
+	curl -s 'https://www.pearsonsuccessnet.com/snpapp/ois/DPSearchTestAction.do' \
+		 -H "Cookie: SNSESSIONID=$SID" \
+		 --data "subcontext=product&step=2&search=true&isbnOID=$ISBN&testTypeOID=$line" \
+		 | sed 's/\t//g' \
+		 | grep 'authTestID="......"' \
+		 | awk -F\" '{print $2}'
+
+done < <(\
+curl -s 'https://www.pearsonsuccessnet.com/snpapp/ois/DPSearchTestAction.do' \
+	 -H "Cookie: SNSESSIONID=$SID" \
+	 --data "step=2&search=true&isbnOID=$ISBN&testTypeOID=-1" \
+	 | grep -A20 'select name="testTypeOID"' \
+	 | sed 's/\t//g' \
+	 | grep '<option value="\(.*\)">' \
+	 | sed 's/<option value="\(.*\)">.*/\1/g' \
+	 | sed -n '2,$p')
